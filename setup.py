eb4c86df255a47d26fa769ab84eeb557149d71be
shimkus <m.shimkus@f5.com>
2019-04-03 10:54:19 -0700

    updated reqs

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+# This should be a list of dependencies required for production use only
+DEPENDENCIES = [
+    'awscli==1.16.136',
+    'azure-mgmt-compute==4.5.1',    
+    'google-cloud==0.34.0'
+]
+
 setup(name='f5-cloud-sdk',
       version='0.9.0',
       description='F5 Cloud SDK',
@@ -8,4 +15,5 @@ setup(name='f5-cloud-sdk',
       author_email='solutionsfeedback@f5.com',
       license='MIT',
       packages=find_packages(exclude=['tests']),
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=DEPENDENCIES)
