80022c85b4d1f1a2291d73a73df1a462e8f110c3
James Sevedge <j.sevedge@f5.com>
2019-04-03 12:03:44 -0700

    minor package and gitignore edits

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+""" setup.py: see public setuptools docs for more detail """
 from setuptools import setup, find_packages
 
 # This should be a list of dependencies required for production use only
@@ -7,13 +8,14 @@ DEPENDENCIES = [
     'google-cloud==0.34.0'
 ]
 
-setup(name='f5-cloud-sdk',
-      version='0.9.0',
-      description='F5 Cloud SDK',
-      url='http://github.com/F5Networks/f5-cloud-sdk',
-      author='F5 Ecosystems Group',
-      author_email='solutionsfeedback@f5.com',
-      license='MIT',
-      packages=find_packages(exclude=['tests']),
-      zip_safe=False,
-      install_requires=DEPENDENCIES)
+setup(
+    name='f5-cloud-sdk',
+    version='0.9.0',
+    description='F5 Cloud SDK',
+    url='http://github.com/F5Networks/f5-cloud-sdk',
+    author='F5 Ecosystems Group',
+    author_email='solutionsfeedback@f5.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=DEPENDENCIES
+)
