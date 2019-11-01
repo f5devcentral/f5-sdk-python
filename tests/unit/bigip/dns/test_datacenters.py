b8930a668c8c39267c0b6c03a772dbb489b29f57
James Sevedge <j.sevedge@f5.com>
2019-11-01 07:49:14 -0700

    add bigip.dns datacenter tests

---
+++ tests/unit/bigip/dns/test_datacenters.py
@@ -0,0 +1,30 @@
+""" Test BIG-IP DNS datacenters client """
+
+from f5cloudsdk.bigip.dns import DataCentersClient
+
+from ....global_test_imports import pytest
+from ....shared import constants
+from ... import utils
+
+REQ = constants.MOCK['requests']
+
+
+class TestDataCentersClient(object):
+    """Test"""
+
+    @staticmethod
+    @pytest.mark.usefixtures("mgmt_client")
+    def test_crud_operations(mgmt_client, mocker):
+        """Test: CRUD operation functions
+
+        Assertions
+        ----------
+        - response should match mocked return value
+        """
+
+        client = DataCentersClient(mgmt_client)
+
+        utils.validate_crud_operations(
+            client,
+            mocker=mocker
+        )
