fcbb800f1ac93ead5abfda3087b3694aa31f08e1
Tuann71 <tuanngo5431@msn.com>
2019-12-11 22:03:14 -0800

    update copy deployment.info and terraform.tfstate to root directory

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,6 +1,5 @@
 """conftest.py for functional tests."""
 
-import os
 import json
 import pytest
 
@@ -10,8 +9,7 @@ from f5cloudsdk.bigip import ManagementClient
 @pytest.fixture(scope="session")
 def management_client():
     """Return management client"""
-    with open(os.path.join(os.path.dirname(__file__),
-                           "../deployment/deployment_info.json")) as json_file:
+    with open("./deployment_info.json") as json_file:
         json_data = json.load(json_file)
         mgmt_client = ManagementClient(json_data['instances'][0]['mgmt_address'],
                                        user=json_data['instances'][0]['admin_username'],
