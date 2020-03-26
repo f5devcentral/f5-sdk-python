6914e191a3a306ce656ffd09a28c5197817e9260
James Sevedge <j.sevedge@f5.com>
2020-03-26 10:47:30 -0700

    [AUTOSDK-196] - Unique Extension Clients

--- f5sdk/bigip/extension/extension.py
+++ f5sdk/bigip/extension/extension.py
@@ -1,28 +1,4 @@
-"""Python module for BIG-IP extension component configuration, including AS3, DO and TS
-
-    Example - Basic::
-
-        from f5sdk.bigip import ManagementClient
-        from f5sdk.bigip.extension import ExtensionClient
-        device = ManagementClient('192.0.2.10', user='admin', password='admin')
-
-        as3 = ExtensionClient(device, 'as3')
-        # install AS3 package
-        as3.package.install()
-        # check service is available
-        as3.service.is_available()
-        # configure AS3
-        as3.service.create(config_file='./decl.json')
-
-    Example - Specify Component Type::
-
-        do = ExtensionClient(device, 'do')
-        ts = ExtensionClient(device, 'ts')
-
-    Example - Specify Component Version::
-
-        as3 = ExtensionClient(device, 'as3', version='3.9.0')
-"""
+"""Python module containing BIG-IP Extension clients"""
 
 from f5sdk.logger import Logger
 
@@ -31,8 +7,8 @@ from .package import OperationClient as PackageClient
 from .service import OperationClient as ServiceClient
 
 
-class ExtensionClient(object):
-    """A class used as a extension client for BIG-IP
+class AS3Client(object):
+    """AS3 client
 
     Attributes
     ----------
@@ -42,15 +18,13 @@ class ExtensionClient(object):
         the extension component version
     """
 
-    def __init__(self, client, component, **kwargs):
+    def __init__(self, client, **kwargs):
         """Class initialization
 
         Parameters
         ----------
         client : object
             the management client object
-        component : str
-            the extension component
         **kwargs :
             optional keyword arguments
 
@@ -70,7 +44,207 @@ class ExtensionClient(object):
 
         self._client = client
         self._metadata_client = MetadataClient(
-            component,
+            'as3',
+            kwargs.pop('version', None),
+            use_latest_metadata=kwargs.pop('use_latest_metadata', False)
+        )
+        self.component = self._metadata_client.component
+        self.version = self._metadata_client.version
+
+    @property
+    def package(self):
+        """ Package (see PackageClient for more details) """
+        return PackageClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+    @property
+    def service(self):
+        """ Service (see ServiceClient for more details)  """
+        return ServiceClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+
+class DOClient(object):
+    """DO client
+
+    Attributes
+    ----------
+    component : str
+        the extension component
+    version : str
+        the extension component version
+    """
+
+    def __init__(self, client, **kwargs):
+        """Class initialization
+
+        Parameters
+        ----------
+        client : object
+            the management client object
+        **kwargs :
+            optional keyword arguments
+
+        Keyword Arguments
+        -----------------
+        version : str
+            a string specifying the component version to use
+        use_latest_metadata : bool
+            use latest metadata (will be retrieved from remote CDN)
+
+        Returns
+        -------
+        None
+        """
+
+        self.logger = Logger(__name__).get_logger()
+
+        self._client = client
+        self._metadata_client = MetadataClient(
+            'do',
+            kwargs.pop('version', None),
+            use_latest_metadata=kwargs.pop('use_latest_metadata', False)
+        )
+        self.component = self._metadata_client.component
+        self.version = self._metadata_client.version
+
+    @property
+    def package(self):
+        """ Package (see PackageClient for more details) """
+        return PackageClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+    @property
+    def service(self):
+        """ Service (see ServiceClient for more details)  """
+        return ServiceClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+class TSClient(object):
+    """TS client
+
+    Attributes
+    ----------
+    component : str
+        the extension component
+    version : str
+        the extension component version
+    """
+
+    def __init__(self, client, **kwargs):
+        """Class initialization
+
+        Parameters
+        ----------
+        client : object
+            the management client object
+        **kwargs :
+            optional keyword arguments
+
+        Keyword Arguments
+        -----------------
+        version : str
+            a string specifying the component version to use
+        use_latest_metadata : bool
+            use latest metadata (will be retrieved from remote CDN)
+
+        Returns
+        -------
+        None
+        """
+
+        self.logger = Logger(__name__).get_logger()
+
+        self._client = client
+        self._metadata_client = MetadataClient(
+            'ts',
+            kwargs.pop('version', None),
+            use_latest_metadata=kwargs.pop('use_latest_metadata', False)
+        )
+        self.component = self._metadata_client.component
+        self.version = self._metadata_client.version
+
+    @property
+    def package(self):
+        """ Package (see PackageClient for more details) """
+        return PackageClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+    @property
+    def service(self):
+        """ Service (see ServiceClient for more details)  """
+        return ServiceClient(
+            self._client,
+            self.component,
+            self.version,
+            self._metadata_client,
+            logger=self.logger
+        )
+
+
+class CFClient(object):
+    """CF client
+
+    Attributes
+    ----------
+    component : str
+        the extension component
+    version : str
+        the extension component version
+    """
+
+    def __init__(self, client, **kwargs):
+        """Class initialization
+
+        Parameters
+        ----------
+        client : object
+            the management client object
+        **kwargs :
+            optional keyword arguments
+
+        Keyword Arguments
+        -----------------
+        version : str
+            a string specifying the component version to use
+        use_latest_metadata : bool
+            use latest metadata (will be retrieved from remote CDN)
+
+        Returns
+        -------
+        None
+        """
+
+        self.logger = Logger(__name__).get_logger()
+
+        self._client = client
+        self._metadata_client = MetadataClient(
+            'cf',
             kwargs.pop('version', None),
             use_latest_metadata=kwargs.pop('use_latest_metadata', False)
         )
--- tests/unit/bigip/extension/conftest.py
+++ tests/unit/bigip/extension/conftest.py
@@ -1,37 +1,54 @@
 """ Test fixtures """
 
-from f5sdk.bigip.extension import ExtensionClient
+import importlib
 
 from ....global_test_imports import pytest
 
 
-@pytest.fixture
+@pytest.fixture(name="get_extension_client_class")
+def get_extension_client_class_fixture():
+    """ Test fixture: Get Extension Client Class (Factory)"""
+
+    def _func(**kwargs):
+        component = kwargs.pop('component', 'as3')
+
+        module = importlib.import_module('f5sdk.bigip.extension')
+
+        if component == 'as3':
+            extension_client_class = getattr(module, 'AS3Client')
+        elif component == 'do':
+            extension_client_class = getattr(module, 'DOClient')
+        elif component == 'ts':
+            extension_client_class = getattr(module, 'TSClient')
+        elif component == 'cf':
+            extension_client_class = getattr(module, 'CFClient')
+        else:
+            raise Exception('Unknown component: {}'.format(component))
+
+        return extension_client_class
+
+    return _func
+
+@pytest.fixture(name="create_extension_client")
+@pytest.mark.usefixtures("get_extension_client_class")
 @pytest.mark.usefixtures("mgmt_client")
-def extension_client(mgmt_client):
-    """ Test fixture: create as3 extension client """
+def create_extension_client_fixture(mgmt_client, get_extension_client_class):
+    """ Test fixture: Create Extension Client (Factory)"""
 
-    return ExtensionClient(mgmt_client, 'as3')
+    def _func(**kwargs):
+        component = kwargs.pop('component', 'as3')
+        version = kwargs.pop('version', None)
+        use_latest_metadata = kwargs.pop('use_latest_metadata', False)
 
+        # define extension client kwargs
+        kwargs = {}
+        if use_latest_metadata:
+            kwargs['use_latest_metadata'] = use_latest_metadata
+        if version is not None:
+            kwargs['version'] = version
 
-@pytest.fixture
-@pytest.mark.usefixtures("mgmt_client")
-def ts_extension_client(mgmt_client):
-    """ Test fixture: create ts extension client """
+        # get extension client class and instantiate
+        extension_client_class = get_extension_client_class(component=component)
+        return extension_client_class(mgmt_client, **kwargs)
 
-    return ExtensionClient(mgmt_client, 'ts')
-
-
-@pytest.fixture
-@pytest.mark.usefixtures("mgmt_client")
-def do_extension_client(mgmt_client):
-    """ Test fixture: create do extension client """
-
-    return ExtensionClient(mgmt_client, 'do')
-
-
-@pytest.fixture
-@pytest.mark.usefixtures("mgmt_client")
-def cf_extension_client(mgmt_client):
-    """ Test fixture: create cf extension client """
-
-    return ExtensionClient(mgmt_client, 'cf')
+    return _func
